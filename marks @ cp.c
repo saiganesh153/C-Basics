826b2a375010529438189a8ed21bbb1731337990
saiganesh153 <92290485+saiganesh153@users.noreply.github.com>
2021-10-25 12:29:36 +0530

    Add files via upload

---
+++ marks @ cp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+ //variable declaration	
+ int phy,che,mat,eng,tot;
+ 
+ //initialization
+ phy = 50;
+ che = 60;
+ mat = 70;
+ eng = 40;
+ 
+ //calculation
+ tot = phy + che + mat + eng;
+ 
+ //output
+ printf("marks in phy,che,mat,eng: %d,%d,%d,%d\n", phy,che,mat,eng);
+ printf("total marks: %d ", tot);
+ 
+}
