696b7b056a3b1f0e738dfbb798f47b6b0762afa7
saiganesh153 <92290485+saiganesh153@users.noreply.github.com>
2021-11-08 12:22:45 +0530

    Add files via upload

---
+++ operaters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+a=20;
+b=10;
+printf("%d>%d ---> %d\n",a,b,a>b);
+printf("%d<%d ---> %d\n",a,b,a<b);
+printf("%d==%d ---> %d\n",a,b,a==b);
+printf("%d!=%d ---> %d\n",a,b,a!=b);
+printf("%d>=%d ---> %d\n",a,b,a>=b);
+printf("%d<=%d ---> %d\n",a,b,a<=b);
+}
