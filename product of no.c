b17746be6b504520c415a6743ba5f092b78a2d90
saiganesh153 <92290485+saiganesh153@users.noreply.github.com>
2021-11-09 12:09:37 +0530

    Add files via upload

---
+++ product of no.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,c,d,p;
+ printf("enter 4 numbers:");
+ scanf("%d%d%d%d",&a,&b,&c,&d);
+ p=a*b*c*d;
+ printf("product of %d %d %d and %d is:%d",a,b,c,d,p);
+ 
+}
