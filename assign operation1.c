e759fa0d6e0cd504e27c2df2fff08222d4b43bee
saiganesh153 <92290485+saiganesh153@users.noreply.github.com>
2021-11-17 12:16:28 +0530

    Add files via upload

---
+++ assign operation1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int a = 10, b = 2;
+ a += b;
+ printf("%d %d\n",a,b);
+ a -= b;
+ printf("%d %d\n",a,b);
+ b *= a;
+ printf("%d %d\n",a,b);
+ b /= a;
+ printf("%d %d\n",a,b);
+ a %= b;
+ printf("%d %d\n",a,b);
+ 
+}
