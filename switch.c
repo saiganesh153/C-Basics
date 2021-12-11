f5487c209ec91eb5c0a2e25f68c00e320e13d3d8
saiganesh153 <92290485+saiganesh153@users.noreply.github.com>
2021-12-11 20:58:17 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("addition of these numbers 2+6");
+	printf("\n1)6\n2)7\n3)8\n4)9\n");
+	scanf("%d", &n);
+	switch(n)
+	{
+		case 1:
+			printf("wrong answer");
+            break;
+            case 2:
+            	printf("wrong answer");
+            	break;
+            	case 3:
+            	    printf("currect answer");
+            	    break;
+                    case 4:
+                    	printf("wrong answer");
+                    	break;
+                    	case 5:
+                    		printf("defalt");
+                    		break;
+        
+    
+	}
+}
